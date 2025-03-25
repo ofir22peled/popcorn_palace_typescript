aadae7787597e2cb0fbeb104e6b22dbbaf902c8c
ofir22peled <ofir22peled@gmail.com>
2025-03-25 16:20:29 +0200

    adding booking module to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './modules/movies/movies.module';
 import { ShowtimesModule } from './modules/showtimes/showtimes.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
 
 @Module({
-  imports: [MoviesModule, ShowtimesModule],
+  imports: [MoviesModule, ShowtimesModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
